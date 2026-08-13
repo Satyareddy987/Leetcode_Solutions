1c9dca4ba6df8c738b1b5b41331c90d4140ddd8d
Satyareddy987 <psatyanarayanareddy722@gmail.com>
2026-08-13 21:41:12 +0530

    Time: 0 ms (100%), Space: 8.4 MB (18.2%) - LeetHub

---
+++ 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int balancedStringSplit(string s) {
+        int l=0,r=0,cnt=0;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='L'){
+                l++;
+            }
+            else{
+                r++;
+            }
+            if(l==r){
+                cnt++;
+                l = 0;
+                r = 0;
+            }
+        }
+        return cnt;
+    }
+};
