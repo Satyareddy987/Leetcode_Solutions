284903e18ecff90bcb8faac74a82f0838b070abf
Satyareddy987 <psatyanarayanareddy722@gmail.com>
2026-08-24 14:34:04 +0530

    Time: 5 ms (5.12%), Space: 8.3 MB (14.04%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int reverseBits(int n) {
+        string s = bitset<32>(n).to_string();
+        reverse(s.begin(),s.end());
+        int res = stoi(s,0,2);
+        return res;
+    }
+};
